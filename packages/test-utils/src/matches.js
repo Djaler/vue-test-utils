9d1c6803c9d63042553c8f137370ba7124cd16ac
Kirill Romanov <djaler1@gmail.com>
2020-11-30 10:55:44 +0300

    fix: use functional component name in vmMatchesName

--- packages/test-utils/src/matches.js
+++ packages/test-utils/src/matches.js
@@ -10,7 +10,7 @@ function vmMatchesName(vm, name) {
   // We want to mirror how Vue resolves component names in SFCs:
   // For example, <test-component />, <TestComponent /> and `<testComponent />
   // all resolve to the same component
-  const componentName = (vm.$options && vm.$options.name) || ''
+  const componentName = vm.name || (vm.$options && vm.$options.name) || ''
   return (
     !!name &&
     (componentName === name ||
